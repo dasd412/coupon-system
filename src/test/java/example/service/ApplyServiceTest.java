71eba7bf8e87521076e5f9658b479fe6f3770b66
yang-yeongjun <dasd412@naver.com>
2024-06-16 17:32:23 +0900

    스레드 레이스 컨디션으로 실패하는 테스트 작성

--- src/test/java/example/service/ApplyServiceTest.java
+++ src/test/java/example/service/ApplyServiceTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import example.repository.CouponRepository;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = CouponSystemApplication.class)
@@ -23,5 +28,34 @@ public class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertEquals(1, count);
+    }
+
+    @Test
+    public void  여러번응모() throws InterruptedException {
+        int threadCount=1000;
+
+        ExecutorService executorService= Executors.newFixedThreadPool(32);
+
+        //다른 스레드가 하는 작업을 기다리도록 도와주는 클래스
+        CountDownLatch latch=new CountDownLatch(threadCount);
+
+        for (int i = 0; i <threadCount ; i++) {
+            long userId=i;
+
+            executorService.submit(()->{
+                try{
+                    applyService.apply(userId);
+                }finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count=couponRepository.count();
+
+        assertEquals(count,100);
+
     }
 }
