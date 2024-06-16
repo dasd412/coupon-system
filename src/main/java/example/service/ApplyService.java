212c6b5b0271b0d440c84ca7b33a15eb0f564869
yang-yeongjun <dasd412@naver.com>
2024-06-16 20:09:41 +0900

    레디스를 활용해 레이스 컨디션 해결

--- src/main/java/example/service/ApplyService.java
+++ src/main/java/example/service/ApplyService.java
@@ -1,6 +1,7 @@
 package example.service;
 
 import example.domain.Coupon;
+import example.repository.CouponCountRepository;
 import org.springframework.stereotype.Service;
 import example.repository.CouponRepository;
 
@@ -8,14 +9,17 @@ import example.repository.CouponRepository;
 public class ApplyService {
 
     private final CouponRepository couponRepository;
+    private final CouponCountRepository couponCountRepository;
 
 
-    public ApplyService(CouponRepository couponRepository) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     public void apply(Long userId){
-        long count=couponRepository.count();
+        // 레디스는 싱글 스레드 기반으로 구현됬음.
+        Long count =couponCountRepository.increment();
 
         if (count>100){
             return;
